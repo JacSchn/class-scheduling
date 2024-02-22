bd86c392a3bb8b2babde8701096fb1dacab87d26
JacSchn <79425177+JacSchn@users.noreply.github.com>
2024-02-21 18:33:17 -0600

    Add files via upload

---
+++ scheduler/views/defaultview.php
@@ -0,0 +1,11 @@
+<div class="text-center mt-3"> <!--class="text-center mt-3"-->
+    <?php
+        echo "<h3>Hello $_SESSION[name],</h3>";
+    ?>
+    <h3>Department of Computer Science<h3>
+    <div class="mt-3"> <!--class="mt-3"-->
+        <!--add image-->
+        <img  src='https://asset.library.wisc.edu/1711.dl/4UFSUXJFTH26X8R/M/native-194d5.jpg?dl' 
+                    height='400px' alt='movies' />
+    </div>
+</div>
